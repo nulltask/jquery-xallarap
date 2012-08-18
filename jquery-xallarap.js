ad6356c9ad7b2ab6565461bed68cca03e89c68d9
nulltask <nulltask@gmail.com>
2012-08-18 21:22:43 +0900

    initial import

---
+++ jquery-xallarap.js
@@ -0,0 +1,62 @@
+
+/*!
+ * jQuery Xallarap 0.0.1
+ *
+ * @dependency jQuery 1.7+
+ * @author Uniba Inc.
+ * @license MIT License
+ */
+
+(function(window, document, $) {
+
+  var $window = $(window)
+    , scrollTop = 0
+    , windowHeight = 0;
+
+  $.fn.xallarap = function(options) {
+    var options = options || {};
+    console.log(this);
+    return this.each(function() {
+      var $this = $(this);
+
+      /**
+       * Store original position and offset.
+       */
+
+      $this
+        .data('xap-position', $this.position())
+        .data('xap-offset', $this.offset())
+        .data('xap-easing', $this.data('xap-easing') || options.easing || 'swing')
+        .data('xap-duration', $this.data('xap-duration') || options.duration || 400)
+        .data('xap-value', $this.data('xap-value') || options.value || 400);
+      
+      /**
+       * Scroll handler.
+       */
+
+      $window.on('scroll', function(e) {
+        var position = $this.data('xap-position')
+          , offset = $this.data('xap-offset')
+          , value = $this.data('xap-value')
+          , percent = (scrollTop - offset.top + windowHeight / 2) / offset.top;
+
+        console.log(percent);
+        $this
+          .stop()
+          .animate({ top: position.top - (value * percent) }, $this.data('duration'), $this.data('easing'));
+      });
+    });
+  };
+
+  /**
+   * Cache window states.
+   */
+
+  $(function() {
+    $(window).on('resize scroll', function(e) {
+      scrollTop = $window.scrollTop();
+      windowHeight = $window.height();
+    }).trigger('resize');
+  });
+
+}).call(this, window, document, jQuery);
